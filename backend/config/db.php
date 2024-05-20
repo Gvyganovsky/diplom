ae0122aebc9a0ce8a2045aa39a5504ffe44381b4
gleb <gvyganovsky@gmail.com>
2024-05-20 07:21:18 +0300

    Настройки config

--- backend/config/db.php
+++ backend/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=322-20_AgroScout',
     'username' => 'root',
-    'password' => '',
+    'password' => '123123123dD@',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
