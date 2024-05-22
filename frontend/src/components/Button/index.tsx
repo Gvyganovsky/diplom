7990f1f753bdc254b0da1f3dbf1c6f5af02ddcb0
gleb <gvyganovsky@gmail.com>
2024-05-22 18:43:44 +0300

    Изменил кнопку

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,27 +1,28 @@
 import styles from "./Button.module.scss";
 
-interface buttonProps {
+interface ButtonProps {
   id?: number;
   title: string;
   link?: string;
   className: string;
-  type?: "button" | "submit" | "reset"; 
+  type?: "button" | "submit" | "reset";
   onClick?: () => void;
 }
 
-const Button: React.FC<buttonProps> = ({ id, title, link, className, type = "button", onClick }) => {
+const Button: React.FC<ButtonProps> = ({ id, title, link, className, type = "button", onClick }) => {
   if (link) {
     return (
-        <a href={`${link}/${id}`} className={`${styles.button} ${className}`}>
+      <a href={`${link}/${id}`} className={`${styles.button} ${className}`}>
         {title}
       </a>
     );
+  } else {
+    return (
+      <button type={type} className={`${styles.button} ${className}`} onClick={onClick}>
+        {title}
+      </button>
+    );
   }
-  return (
-    <button type={type} className={`${styles.button} ${className}`} onClick={onClick}>
-      {title}
-    </button>
-  );
 };
 
 export default Button;
