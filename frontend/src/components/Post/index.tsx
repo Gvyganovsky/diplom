5c1173aeccad300bb207497c0d0d6b1a68c4e94d
gleb <gvyganovsky@gmail.com>
2024-05-23 15:30:47 +0300

    contact

--- frontend/src/components/Post/index.tsx
+++ frontend/src/components/Post/index.tsx
@@ -13,11 +13,22 @@ const index = (props: any) => {
           <p className={styles.description}>{props.description1}</p>
           <p className={styles.description}>{props.description2}</p>
           <p className={styles.description}>{props.description3}</p>
-          <div>
-            <img src="" alt="" />
-            <p>{props.enemy}</p>
-            <ul>фыв</ul>
-          </div>
+
+          <>
+            <div className={styles.enemyBlock}>
+              <img src="/iconBatterfly.svg" alt="Batterfly" width={40} height={40} className={styles.icon} />
+              <p>{props.enemy} эффективна против таких вредителей:</p>
+            </div>
+
+            <ul className={styles.list}>
+              {props.postData.map((post, index) => (
+                <li className={styles.item} key={index}>
+                  <img src="/IconCheck.svg" alt="IconCheck" width={18} height={18} className={styles.iconCheck} />
+                  <p className={styles.postText}>{post.text}</p>
+                </li>
+              ))}
+            </ul>
+          </>
         </div>
       </div>
     </div>
--- frontend/src/pages/Contact/index.tsx
+++ frontend/src/pages/Contact/index.tsx
@@ -1,32 +1,34 @@
-import stylesContact from "./Contact.module.scss";
-import stylesApp from "../../App.module.scss";
+import styles from "./Contact.module.scss";
 import Button from "../../components/Button";
+import Title from "../../components/Title";
+import Breadcrumbs from "../../components/Breadcrumbs";
 
 const index = () => {
   return (
-    <section className={stylesContact.contact}>
-      <div className={stylesApp.container}>
-        <p className={stylesApp.router}>
-          Главная \ <span className={stylesApp.router_green}>Контакты</span>
-        </p>
-        <h1 className={stylesApp.title}>Как с нами связаться</h1>
-        <div className={stylesContact.content}>
-          <p className={stylesContact.text__vert}>КОНТАКТЫ</p>
-          <div className={stylesContact.info}>
-            <h2 className={stylesContact.title}>AgroScout</h2>
-            <p className={stylesContact.text__small}>Наш адрес</p>
-            <p className={stylesContact.text__normal}>Россия, г. Москва</p>
-            <p className={stylesContact.text__small}>Наш телефон</p>
-            <p className={stylesContact.text__normal}>+7(861)-217-91-18</p>
+    <section className={styles.contact}>
+      <div className={styles.container}>
+        <Breadcrumbs title="Контакты" />
+
+        <Title text="Как с нами связаться" />
+
+        <div className={styles.content}>
+          <p className={styles.text__vert}>КОНТАКТЫ</p>
+          <div className={styles.info}>
+            <h2 className={styles.title}>AgroScout</h2>
+            <p className={styles.text__small}>Наш адрес</p>
+            <p className={styles.text__normal}>Россия, г. Москва</p>
+            <p className={styles.text__small}>Наш телефон</p>
+            <p className={styles.text__normal}>+7(861)-217-91-18</p>
             <img
               src="./iconDrone_flying.svg"
               alt="Drone flying"
-              className={stylesContact.img}
+              className={styles.img}
               height={130}
             />
             <Button title="Написать нам" className="buttonWhite" />
           </div>
-          <img src="./map.jpg" alt="map" width={1130} height={510}/>
+          
+          <img src="./map.jpg" alt="map" width={1000} height={510} className={styles.map} />
         </div>
       </div>
     </section>
